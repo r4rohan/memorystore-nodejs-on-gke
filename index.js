3fe3f6ce6875da9a2942faf09ef482c2f0c173cd
Nitish Kumar Singh <nitishk72@gmail.com>
2020-05-02 17:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,29 +13,57 @@ const REDISPORT = process.env.REDISPORT || 6379;
 const redis_client = redis.createClient(REDISPORT, REDISHOST);
 redis_client.on('error', (err) => console.error('ERR:REDIS:', err));
 
-//configure express server
 const app = express();
 
-//Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-// app.get("/api/", (req, res) => res.send ('api working'))
+
+app.get('/api/user/:uid',function(req,res){
+  const {uid} = req.params;
+
+  return redis_client.get(uid,function(err,val){
+    let value = val;
+    let isNew  = false;
+    if(!value){
+      isNew = true;
+      value = Math.floor(Math.random() * 100);
+      redis_client.setex(uid,60, value);
+    }
+
+    return res.send({
+      value,
+      isNew
+    })
+
+  });
+
+});
+
 app.get("/api/playerStats/:apikey&:pid", async (req, res) => {
   try {
     const { pid, apikey } = req.params;
-    const url = `https://cricapi.com/api/playerStats?${apikey}&${pid}`
-    const playerInfo = await axios.get(url);
-
-    //get data from response
-    const playerInfoData = playerInfo.data;
-
-    return res.json(playerInfoData);
+    return redis_client.get(pid,async function(err, val){
+      let value = val;
+      let isNew = 'from redis';
+      if(!value){
+        const url = `https://cricapi.com/api/playerStats?${apikey}&${pid}`
+        const playerInfo = await axios.get(url);
+        const playerInfoData = playerInfo.data;
+        value = JSON.stringify(playerInfoData);
+        redis_client.setex(pid,30,val);
+        isNew = 'not from redis';
+      }
+      const data = JSON.parse(value);
+      return res.send({data, isNew});
+            
+    })
   } catch (error) {
     console.log(error);
     return res.status(500).json(error);
   }
 });
+
 
 app.listen(8080);
