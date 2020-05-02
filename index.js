291344397b85f31ffb84c909e59594549f89bed7
Nitish Kumar Singh <nitishk72@gmail.com>
2020-05-02 17:46:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.get('/api/user/:uid', function (req, res) {
+  const { uid } = req.params;
+
+  return redis_client.get(uid, function (err, val) {
+    let value = val;
+    let isNew = false;
+    if (!value) {
+      isNew = true;
+      value = Math.floor(Math.random() * 100);
+      redis_client.setex(uid, 60, value);
+    }
+    return res.send({ value, isNew })
+  });
+});
+
 app.get("/api/playerStats/:apikey&:pid", async (req, res) => {
   try {
     const { pid, apikey } = req.params;
@@ -29,7 +45,7 @@ app.get("/api/playerStats/:apikey&:pid", async (req, res) => {
         const playerInfo = await axios.get(url);
         const playerInfoData = playerInfo.data;
         value = JSON.stringify(playerInfoData);
-        redis_client.setex(pid,30,val);
+        redis_client.setex(pid,30,value);
         isNew = 'not from redis';
       }
       const data = JSON.parse(value);
