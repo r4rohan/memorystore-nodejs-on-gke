2c663ec622accbfa9056cbe4684142ea3d1a9858
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2020-05-03 04:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const redis = require('redis');
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+
 // ports
 const REDISHOST = process.env.REDISHOST || 'localhost';
 const REDISPORT = process.env.REDISPORT || 6379;
 const PORT = process.env.PORT || 8080;
-const VALID_TILL_SECONDS = 60;
-const VERSION_NUMBER = process.env.VERSION || 4;
 
 const redis_client = redis.createClient(REDISPORT, REDISHOST);
 redis_client.on('error', (err) => console.error('ERR:REDIS:', err));
@@ -17,33 +16,45 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send(`App is running ${VERSION_NUMBER}`);
+app.get('/', function(req,res){
+  res.send("ROOT of app");
+})
+
+app.get('/api', function(req,res){
+  res.send("API is working");
+})
+
+app.get('/api/env', function(req,res){
+  res.send({
+    REDISHOST,
+    REDISPORT,
+    PORT
+  });
 })
 
 app.get('/api/user/:uid', function (req, res) {
   const { uid } = req.params;
   return redis_client.get(uid, function (err, val) {
     let value = val;
-    let source = 'REDIS';
+    let isNew = false;
     if (!value) {
-      source = 'NOT REDIS';
-      value = getMagicNumber();
-      redis_client.setex(uid, VALID_TILL_SECONDS, value);
+      isNew = true;
+      value = Math.floor(Math.random() * 100);
+      redis_client.setex(uid, 60, value);
     }
-    return res.send({ source, value })
+    return res.send({ value, isNew })
   });
 });
 
-app.get("/api/playerStats/:apikey&:pid", async (req, res) => {
+app.get("/type/:pid", async (req, res) => {
   try {
-    const { pid, apikey } = req.params;
+    const { pid } = req.params;
     return redis_client.get(pid, async function (err, val) {
       let value = val;
       let Source = 'REDIS';
       if (!value) {
-        value = await getDatafromSource(apikey, pid);
-        redis_client.setex(pid, VALID_TILL_SECONDS, value);
+        value = await getDatafromSource(pid);
+        redis_client.setex(pid, 60, value);
         Source = 'API';
       }
       const data = JSON.parse(value);
@@ -56,14 +67,12 @@ app.get("/api/playerStats/:apikey&:pid", async (req, res) => {
   }
 });
 
-function getMagicNumber() {
-  return Math.floor(Math.random() * 100);
-}
-async function getDatafromSource(apikey, pid) {
-  const url = `https://cricapi.com/api/playerStats?${apikey}&${pid}`
-  const playerInfo = await axios.get(url);
-  const playerInfoData = playerInfo.data;
-  stringResponse = JSON.stringify(playerInfoData);
+async function getDatafromSource(pid) {
+  const url = `https://pokeapi.co/api/v2/type/${pid}`
+  const pokemonInfo = await axios.get(url);
+  const pokemonInfoData = pokemonInfo.data;
+  stringResponse = JSON.stringify(pokemonInfoData);
   return stringResponse
 }
+
 app.listen(PORT);
